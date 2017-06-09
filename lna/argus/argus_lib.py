6c775e40c2d2a7e393053b1bfe9a3c5621153da7
dehu4ka@gmail.com <huiv0ine!>
2017-06-10 01:07:11 +0500

    adsl upload to db is done

--- lna/argus/argus_lib.py
+++ lna/argus/argus_lib.py
@@ -1,18 +1,80 @@
 import csv
+from django.db import connection
+from .models import ArgusADSL
+import re
 
+tel_pattern = re.compile("\((\d+)\)(\d+)")
+ip_pattern = re.compile("[(\[](10.+)[)\]]")  # Все IP начинаются на 10
+
+def tel_num_to_int(tel_num):
+    """
+    Преобразует телефонный номер из вида (34936)30005 в 3493630005
+    :param tel_num: телефонный номер со скобками на входе
+    :return: телефонный номер в виде числа со скобками на выходе
+    """
+    m = re.search(tel_pattern, tel_num)
+    if m:
+        return int(m.group(1)+m.group(2))
+    return -1
+
+
+def parse_iptv_login(raw_login):
+    """
+    Преобразует IPTV-логин в нормальный вид
+    :param raw_login: IP-TV Smart TUBE089004932
+    :return: 77089004932
+    """
+    return '77'+raw_login[16:]
+
+
+def parse_inet_login(raw_login):
+    """
+    Преобразует Интернет-логин в нормальный вид
+    :param raw_login: СПД893623347
+    :return: 77893623347
+    """
+    return '77' + raw_login[3:]
+
+
+def get_ne_ip_from_hostname(hostname):
+    m = re.search(ip_pattern, hostname)
+    if m:
+        return m.group(1)
+    return "0.0.0.0"
 
 def parse_adsl_csv(filename):
-    print("Trying to PARSE ADSL with file: "+filename)
+    cursor = connection.cursor()
+    cursor.execute("TRUNCATE argus_ArgusADSL CASCADE;")
+
+    # По одному объекты вставляются мееедленно. Будем вставлять пачкой
+    records = list()
+
     # some filename hardcoding...
     with open('lna'+filename, encoding='windows-1251') as csv_file:
         reader = csv.reader(csv_file, delimiter=';', quotechar='"')
         next(reader)
         next(reader)
         next(reader)
-        next(reader)  # Первые четыре строчки неинтересные.
+        next(reader)  # Первые четыре строчки неинтересные. (Заголовки CSV)
         for row in reader:
-            print(row)
-            break
+            client = ArgusADSL()
+            client.city = row[1]
+            client.hostname = row[2]
+            client.ne_ip = row[3]
+            if client.ne_ip == '':
+                client.ne_ip = get_ne_ip_from_hostname(client.hostname)
+            client.tel_num = tel_num_to_int(row[4])
+            client.address = row[5]
+            client.fio = row[6]
+            client.room = row[7]
+            client.iptv_login = parse_iptv_login(row[8])
+            client.inet_login = parse_inet_login(row[9])
+            client.xdsl_slot = row[10]
+            client.xdsl_port = row[11]
+            records.append(client)
+            # break
+        # Массовое добавление
+        ArgusADSL.objects.bulk_create(records, batch_size=1000)
     pass
 
 
--- lna/argus/models.py
+++ lna/argus/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ArgusADSL(models.Model):
+    city = models.CharField(max_length=512, default='N/A')
+    hostname = models.CharField(max_length=512, default='None')
+    ne_ip = models.GenericIPAddressField(protocol='IPv4', default='0.0.0.0')
+    tel_num = models.BigIntegerField(default=-1, db_index=True)
+    address = models.CharField(max_length=512, default='N/A')
+    fio = models.CharField(max_length=512, default='N/A')
+    room = models.CharField(max_length=512, default='N/A')
+    iptv_login = models.CharField(max_length=512, db_index=True)
+    inet_login = models.CharField(max_length=512, db_index=True)
+    xdsl_slot = models.CharField(max_length=512, default='N/A')
+    xdsl_port = models.CharField(max_length=512, default='N/A')
+
