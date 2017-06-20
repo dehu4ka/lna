6afdd843facd416150a584c443e4442f16147324
dehu4ka <dehu4ka@gmail.com>
2017-06-20 18:25:37 +0500

    pick-ne vendor select is working

--- lna/static/js/pick-ne.js
+++ lna/static/js/pick-ne.js
@@ -2,27 +2,60 @@
 $(document).ready(function($) {
     // get ready for multiselect
 
-    // get json data
-    $.getJSON('/api/ne_list/', function (data) {
-        //console.log(data);
-        // array of NE's
-        var multiselect_text = '';
-        data.forEach(function (ne) {
-            //console.log(ne.hostname + "(" + ne.ne_ip + ")");
-            multiselect_text += '<option value="' + ne.id + '"' +
-                    ' data-vendor="' + ne.vendor + '"' +
-                    ' data-model="' + ne.model +'"' +
-                '>' +
-                ne.hostname + "(" + ne.ne_ip + ")" + '</option>'
+    var multiselect = $('#multiselect');
+
+    // get json data about NE list and populate select
+    function get_and_place_NE() {
+        $.getJSON('/api/ne_list/', function (data) {
+            // array of NE's
+            var multiselect_text = '';
+            data.forEach(function (ne) {
+                multiselect_text += '<option value="' + ne.id + '"' +
+                        ' data-vendor="' + ne.vendor + '"' +
+                        ' data-model="' + ne.model +'"' +
+                    '>' +
+                    ne.hostname + "(" + ne.ne_ip + ")" + '</option>'
+            });
+            multiselect.html(multiselect_text);
         });
-        // console.log(multiselect_text)
-        $("#multiselect").html(multiselect_text);
+    }
+
+    get_and_place_NE();
+
+    // vendor list, get and populate select
+    $.getJSON('/api/vendors/', function (data) {
+        var vendors_text = '<option value="All">All</option>';
+        data.forEach(function (vendor) {
+            vendors_text += '<option value="' + vendor.vendor +
+                    '">' + vendor.vendor + '</option>'
+        });
+        $('#vendors_select').html(vendors_text)
     });
 
-
-
     // multiselect prepare form
-    $('#multiselect').multiselect();
+    multiselect.multiselect();
+
+
+    $('#vendors_select').change(function () {
+        //console.log('was changed to ' + this.value)
+        var filter = $(this).val();
+        filterSelect(filter)
+    });
+
+    function filterSelect(value) {
+        console.log('filter select - ' + value);
+        var list = $("#multiselect option");
+        if (value === 'All') {
+            //list.show();
+            get_and_place_NE()
+        } else {
+            list.hide();
+            $(".multiselect").find("option[data-vendor*=" + value + "]").each(function (i) {
+                $(this).show();
+            });
+        }
+
+    }
 
 
 });
