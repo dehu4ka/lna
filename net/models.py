8dac64cdbf8ab5d45eadfb346aaec66eefa57601
dehu4ka <dehu4ka@gmail.com>
2017-06-21 11:15:57 +0500

    some new models

--- net/models.py
+++ net/models.py
@@ -8,9 +8,27 @@ class Job(models.Model):
     created = models.DateTimeField(default=timezone.now)
     completed = models.DateTimeField(null=True)
     celery_id = models.CharField(max_length=255)
+    script_name = models.CharField(max_length=255, default='')
+    ne_ip = models.GenericIPAddressField(protocol='IPv4', default='0.0.0.0')
 
     class Meta:
         ordering = ('created', )
 
     def __str__(self):
         return self.name
+
+
+class JobResult(models.Model):
+    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
+    result = models.TextField(max_length=10000)
+
+
+class Scripts(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=10000)
+    class_name = models.CharField(max_length=255)
+    possible_vendor = models.CharField(max_length=255)
+    possible_model = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name', )
