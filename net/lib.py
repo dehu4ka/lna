ca7a0c3131f13b542b7cda03386ad6b679760bff
dehu4ka@gmail.com <dehu4ka@gmail.com>
2017-06-26 23:58:56 +0500

    active tasks completed

--- net/lib.py
+++ net/lib.py
@@ -2,6 +2,7 @@ from argus.models import ASTU
 from django.core import serializers
 import json
 from net.models import Job, JobResult
+from django.utils import timezone
 
 from rest_framework import serializers
 
@@ -15,11 +16,14 @@ def update_job_status(celery_id=None, state=None, meta=None, result=None):
     job = Job.objects.get(celery_id=celery_id)
     if state:
         job.status = state
-    if result:
-        job_result = JobResult.objects.get_or_create(job_id=job)
-        job_result.result = result
-        job_result.save()
+        if state == 'SUCCESS':
+            job.completed = timezone.now()
     if meta:
         job.meta = meta
     job.save()
+    if result:
+        job_result, created = JobResult.objects.get_or_create(job_id=job)
+        job_result.result = result
+        job_result.save()
+
 
--- net/scripts/long.py
+++ net/scripts/long.py
@@ -13,7 +13,8 @@ def start(self, *args, **kwargs):
         time.sleep(1)
         log.warning('Tick %s of 10' % str(i))
         self.update_state(states.STARTED, meta={'current': i, 'total': 30})
-        update_job_status(self.request.id, meta={'current': i, 'total': 30})
+        update_job_status(self.request.id, state=states.STARTED, meta={'current': i, 'total': 30})
+    # meta is JSON field, it cant' be empty
+    update_job_status(self.request.id, state=states.SUCCESS, meta='{}', result='My Mega Long Result')
     self.update_state(states.SUCCESS)
-    update_job_status(self.request.id, state=states.SUCCESS, meta=None)
     return {"status": "Long Task completed", "num_of_seconds": 30}
