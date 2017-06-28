d81f8030d1cfd0d0e0f5b502cbfcc01225c82636
dehu4ka <dehu4ka@gmail.com>
2017-06-28 11:34:57 +0500

    .

---
+++ lna/static/js/tasks.js
@@ -0,0 +1,15 @@
+function refresh_task(celery_id) {
+    console.log("Refresing Task status for " + celery_id)
+}
+
+function terminate_task(celery_id) {
+
+}
+
+function delete_task(celery_id) {
+
+}
+
+function task_result(celery_id) {
+
+}
